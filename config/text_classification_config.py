791cba8e9ad87df5eefa02bbb82943e7dc66105f
ljw23 <596951616@qq.com>
2020-08-28 17:17:53 +0800

    rebuild torch classification

---
+++ config/text_classification_config.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   text_classification_config.py
+@Time    :   2020/08/28 16:46:54
+@Author  :   Liu Junwen 
+@Version :   1.0
+@Contact :   ljw23@mail.ustc.edu.cn
+@Desc    :   None
+'''
+
+# here put the import lib
+from dataclasses import dataclass, field
+
+
+@dataclass
+class GlueDataTrainingArguments:
+    """
+    Arguments pertaining to what data we are going to input our model for training and eval.
+
+    Using `HfArgumentParser` we can turn this class
+    into argparse arguments to be able to specify them on
+    the command line.
+    """
+
+    task_name: str = field(metadata={"help": "The name of the task to train on: " + ", ".join(glue_processors.keys())})
+    data_dir: str = field(
+        metadata={"help": "The input data dir. Should contain the .tsv files (or other data files) for the task."}
+    )
+    max_seq_length: int = field(
+        default=128,
+        metadata={
+            "help": "The maximum total input sequence length after tokenization. Sequences longer "
+            "than this will be truncated, sequences shorter will be padded."
+        },
+    )
+    overwrite_cache: bool = field(
+        default=False, metadata={"help": "Overwrite the cached training and evaluation sets"}
+    )
+
+    def __post_init__(self):
+        self.task_name = self.task_name.lower()
